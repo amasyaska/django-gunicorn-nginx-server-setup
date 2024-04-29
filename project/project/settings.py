fe321a59040a6de872e87d3e7f4d6dfaa354531b
amasyaska <bogdan.kolesnikov12345@gmail.com>
2024-04-30 00:02:35 +0300

    created .env file with SECRET_KEY and DEBUG values, changed settings.py to get values from env.

--- project/project/settings.py
+++ project/project/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -20,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qt2n2k^l-gm^!^(+n8$@xqucrhlc7$%b4q!^a!%e^slr26lp@s'
+# SECRET_KEY = 'django-insecure-qt2n2k^l-gm^!^(+n8$@xqucrhlc7$%b4q!^a!%e^slr26lp@s'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
